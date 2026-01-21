872c1121a58773f55ce3ff0f384ae43cbfc82aec
BALAMURUGAN V <balamuruganvau@outlook.com>
2026-01-21 14:39:38 +0530

    Update Python files

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,6 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from services import search_service
 
-app = FastAPI(title= "Search Service")
+app = FastAPI(title="Search Service")
 app.include_router(search_service)
-app.add_middleware(CORSMiddleware, allow_origins="*", allow_headers=["Content-Type", "Authorization", "Access-Control-Allow-Credentials"], allow_credentials= True)
-
+app.add_middleware(CORSMiddleware, allow_origins="*", allow_headers=["Content-Type", "Authorization", "Access-Control-Allow-Credentials"], allow_credentials=True)
